b433df53517cdd270716ad9636bf5cfbe202501f
Vivek Bhatt <vivekbhatt618@gmail.com>
2023-06-08 08:03:08 -0700

    STYLE: Page Wrapper

--- src/Components/Layout/PageWrapper/index.jsx
+++ src/Components/Layout/PageWrapper/index.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 const PageWrapper = (props) => {
   const { children, className } = props;
-  const classes = className + " max-w-7xl m-auto";
+  const classes =
+    className +
+    " max-w-7xl m-auto flex flex-col mx-3 my-4 sm:mx-8 sm:my-6 md:mx-12 md:my-8 lg:mx-14 lg:my-8";
   return <div className={classes}>{children}</div>;
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,6 @@ module.exports = {
       md: "768px",
       lg: "976px",
       xl: "1440px",
-      "2xl": "1536px",
     },
     colors: {},
     fontFamily: {
