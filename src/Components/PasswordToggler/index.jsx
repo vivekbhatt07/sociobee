50c9b79ed6bd29b92faa532bdce12a26bc79d1bb
Vivek Bhatt <vivekbhatt618@gmail.com>
2023-06-11 04:37:06 -0700

    STYLE: input & passtoggle

--- src/Components/PasswordToggler/index.jsx
+++ src/Components/PasswordToggler/index.jsx
@@ -8,9 +8,10 @@ const PasswordToggler = (props) => {
     <div className={classes}>
       {children}
       <button
-        className=" absolute z-10 p-2 right-2 top-[6px] w-8 h-8 rounded-full flex justify-center items-center transition-all hover:bg-stone-300"
+        className=" absolute z-10 p-2 right-2 top-[6px] w-8 h-8 rounded-full flex justify-center items-center transition-all duration-300 hover:bg-stone-300"
         type="button"
         onClick={(event) => {
+          event.preventDefault();
           handleVisibility(!isTypePassword);
         }}
       >
