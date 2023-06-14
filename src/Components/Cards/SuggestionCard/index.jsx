509f853eafd99e603414ad4ee3db5bf3e486f61d
Vivek Bhatt <vivekbhatt618@gmail.com>
2023-06-14 14:29:54 -0700

    STYLE: Responsiveness Done

--- src/Components/Cards/SuggestionCard/index.jsx
+++ src/Components/Cards/SuggestionCard/index.jsx
@@ -4,8 +4,8 @@ import { Add } from "@mui/icons-material";
 
 const SuggestionCard = () => {
   return (
-    <article className="flex justify-between items-center p-2">
-      <div className="flex gap-3">
+    <article className="flex p-2 lg:flex-col lg:items-start lg:gap-2 xl:flex-row xl:justify-between xl:items-center">
+      <div className="flex gap-3 lg:justify-start lg:w-full xl:justify-start xl:gap-3">
         <AvatarActionLink avatar="https://res.cloudinary.com/dtrjdcrme/image/upload/v1651473734/socialmedia/avatars/adarsh-balika_dct6gm.webp" />
         <div className="flex flex-col">
           <span>John Doe</span>
--- src/Components/Tab/index.jsx
+++ src/Components/Tab/index.jsx
@@ -24,13 +24,13 @@ function Tab(props) {
     <div className="tab min-h-screen">
       <Header className="tab_header" />
       <div className="tab_nav flex justify-center">
-        <ul className="flex flex-row w-60 justify-center p-3 lg:flex-col list-none bg-[#fff] dark:bg-stone-950">
+        <ul className="flex flex-row w-60 justify-center p-3 lg:flex-col list-none bg-[#fff] dark:bg-stone-950 md:flex-col md:h-[80dvh] md:justify-start lg:p-0 xl:w-full">
           {tabList.map((currentTab, i) => {
             const { name, logo, reach } = currentTab;
             return (
               <li className="tab_nav_item flex" key={i}>
                 <Link
-                  className="tab_btn text-base flex w-14 h-14 justify-center items-center gap-4 no-underline font-sans text-stone-950 font-semibold transition-colors duration-300 dark:text-stone-50 bg-stone-50 border-0 dark:bg-stone-950 hover:bg-stone-400 hover:text-stone-950"
+                  className="tab_btn text-base flex w-14 h-14 justify-center items-center gap-4 no-underline font-sans text-stone-950 font-semibold transition-colors duration-300 dark:text-stone-50 bg-stone-50 border-0 dark:bg-stone-950 hover:bg-stone-400 hover:text-stone-950 md:w-full lg:justify-start"
                   onClick={() => setActiveTab(i)}
                   to={reach}
                   style={{
@@ -40,7 +40,7 @@ function Tab(props) {
                   }}
                 >
                   {activeTab === i ? logo.filled : logo.outlined}{" "}
-                  <span className="tab_btn_label hidden">{name}</span>
+                  <span className="tab_btn_label hidden lg:block">{name}</span>
                 </Link>
               </li>
             );
@@ -50,7 +50,7 @@ function Tab(props) {
       <div className="tab_outlet border-l bg-[#fff] dark:bg-stone-950">
         {tabList[activeTab].component}
       </div>
-      <div className="tab_sidebar bg-[#fff] dark:bg-stone-950 lg:overflow-y-scroll lg:h-[90vh] scroll-smooth">
+      <div className="tab_sidebar bg-[#fff] dark:bg-stone-950 lg:overflow-y-scroll scroll-smooth">
         <div className="flex flex-col gap-2 p-3">
           <div className="flex w-full gap-2">
             <OutlinedActionBtn
@@ -67,7 +67,7 @@ function Tab(props) {
               <span>Latest</span>
             </OutlinedActionBtn>
           </div>
-          <div className="hidden flex-col gap-2">
+          <div className="hidden flex-col gap-2 lg:block">
             <span className="text-lg">Suggestions for you</span>
             <div className="flex flex-col gap-2">
               <SuggestionCard />
