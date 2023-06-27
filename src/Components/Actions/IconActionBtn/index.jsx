69f2d26aa44c61ff760cf00ee1657570775e90d7
Vivek Bhatt <vivekbhatt618@gmail.com>
2023-06-28 04:58:19 +0530

    STYLE: user profile

--- src/Components/Actions/IconActionBtn/index.jsx
+++ src/Components/Actions/IconActionBtn/index.jsx
@@ -4,7 +4,7 @@ const IconActionBtn = (props) => {
   const { className, children, iconBtnType, handleClick } = props;
   const classes =
     className +
-    " w-8 h-8 rounded-full flex justify-center items-center transition-all duration-300 hover:bg-stone-300 dark:text-stone-950 dark:hover:bg-stone-700";
+    " w-8 h-8 text-stone-950 rounded-full flex justify-center items-center transition-all duration-300 hover:bg-stone-300 dark:text-stone-50 dark:hover:bg-stone-700";
   return (
     <button className={classes} type={iconBtnType} onClick={handleClick}>
       {children}
