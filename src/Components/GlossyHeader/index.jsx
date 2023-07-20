4cc325181042e7f0521719c7b60826a0d56bfaa4
Vivek Bhatt <vivekbhatt618@gmail.com>
2023-07-20 05:39:31 +0530

    STYLE: Glossy Header Btn

--- src/Components/GlossyHeader/index.jsx
+++ src/Components/GlossyHeader/index.jsx
@@ -9,9 +9,12 @@ const GlossyHeader = (props) => {
   return (
     <div className="p-4 sticky top-0 backdrop-blur-md z-10 border-b border-darkGrey dark:border-lightGrey flex items-center justify-start gap-4">
       {isArrow && (
-        <IconActionBtn handleClick={() => navigate(-1)}>
+        <button
+          onClick={() => navigate(-1)}
+          className="w-8 h-8 text-stone-700 rounded-full flex justify-center items-center transition-all duration-300 hover:bg-stone-300 active:bg-stone-400 dark:text-stone-50 dark:hover:bg-stone-400 dark:active:bg-stone-500"
+        >
           <ArrowBackIcon className="text-stone-700" />
-        </IconActionBtn>
+        </button>
       )}
       <span className="text-lg text-stone-600 font-semibold dark:text-stone-400">
         {title}
